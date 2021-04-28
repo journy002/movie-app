c5b455b453c78c4b15cb4faac0fbf40aa8db65fc
journy002 <journy002@naver.com>
2021-04-28 21:22:18 +0900

    study appjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// const foodILike = [
+//   {
+//     id:1,
+//     name:'kimchi',
+//     rating: 8
+//   },
+//   {
+//     id:2,
+//     name:'sogogi',
+//     rating: 10
+//   },
+//   {
+//     id:3,
+//     name:'sishi',
+//     rating: 8
+//   }
+// ]
+
+// function Food ({name, rating}){
+// return (
+//   <div>
+//     <h2>I really like eat {name}</h2>
+//     <h3>{rating}/10</h3>
+//   </div>
+//   )
+// }
+
+// check props
+// Food.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   rating: PropTypes.number.isRequired
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       {foodILike.map(dish => (
+//         <Food key={dish.id} name={dish.name} rating={dish.rating} />
+//       ))}
+//     </div>
+//   );
+// }
+
+class App extends React.Component{
+  render(){
+    return <h1>I have render component</h1>
+  }
 }
 
 export default App;
