9db7d059a6ecb9ad4978b07bfc1ea3bc213cc2cf
journy002 <journy002@naver.com>
2021-04-28 22:05:12 +0900

    study react count

--- src/App.js
+++ src/App.js
@@ -2,52 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
-// const foodILike = [
-//   {
-//     id:1,
-//     name:'kimchi',
-//     rating: 8
-//   },
-//   {
-//     id:2,
-//     name:'sogogi',
-//     rating: 10
-//   },
-//   {
-//     id:3,
-//     name:'sishi',
-//     rating: 8
-//   }
-// ]
-
-// function Food ({name, rating}){
-// return (
-//   <div>
-//     <h2>I really like eat {name}</h2>
-//     <h3>{rating}/10</h3>
-//   </div>
-//   )
-// }
-
-// check props
-// Food.propTypes = {
-//   name: PropTypes.string.isRequired,
-//   rating: PropTypes.number.isRequired
-// }
-
-// function App() {
-//   return (
-//     <div>
-//       {foodILike.map(dish => (
-//         <Food key={dish.id} name={dish.name} rating={dish.rating} />
-//       ))}
-//     </div>
-//   );
-// }
-
 class App extends React.Component{
+  
+  state = {
+    count: 0
+  };
+
+  add = () => {
+    this.setState(current => ({count: current.count + 1}));
+  }
+
+  minus = () => {
+    this.setState(current => ({count: current.count - 1}));
+  }
+
+  componentDidMount(){
+    console.log('component rendered');
+  }
+  componentDidUpdate(){
+    console.log('i just updating');
+  }
+
   render(){
-    return <h1>I have render component</h1>
+    console.log("i'm randering");
+    return (
+        <div>
+          <h1>This number is: {this.state.count}</h1>
+          <button onClick={this.add}>add</button>
+          <button onClick={this.minus}>minus</button>
+        </div>
+      )
   }
 }
 
