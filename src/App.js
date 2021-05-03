f03000782c77041bbaeef8526d48e7a0266ec0a7
journy002 <journy002@naver.com>
2021-05-03 14:19:47 +0900

    movie app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React from 'react';
-import { useEffect, useState }  from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 
-const App = () => {
-  const [count, setCount] = useState(0);
+class App extends React.Component{
+  state = {
+    isLoading : true,
+    movies : []
+  }
 
-  useEffect(() => {
-    if(count < 0) alert('0 이하 입니다');
-  },[count]);
-  
-  return (
-    <div>
-      <h1>This number is: {count}</h1>
-      <button onClick={setCount(count + 1)}>add</button>
-      <button onClick={setCount(count - 1)}>minus</button>
+  getMovies = async () => {
+    const {data: {data: {movies}}} = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    this.setState({movies, isLoading: false})
+  }
+
+  componentDidMount(){
+    this.getMovies();
+  }
+
+  render(){
+    const {isLoading, movies} = this.state;
+    return <div>
+    {isLoading ? 'Loading...??' : movies.map(movie => 
+    <Movie 
+     key ={movie.id}
+     id={movie.id}
+     year={movie.year}
+     title={movie.title} 
+     summary={movie.summary} 
+     poster={movie.medium_cover_image} />
+    )}
     </div>
-  )
+  }
 }
 
 export default App;
